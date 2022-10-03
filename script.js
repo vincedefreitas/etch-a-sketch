f748dc4a50f5872bb4e4a6ab30edce6aa9fe3aee
Vince <vince.defreitas@gmail.com>
2022-10-03 11:45:14 +0100

    Add grid size functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const gridButton = document.querySelector('.resize-grid');
 
 function makeGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows)
@@ -10,9 +11,34 @@ function makeGrid(rows, cols) {
     }
 }
 
+function changeGridSize() {
+    let nodes = document.querySelectorAll('.square');
+    for (i = 0; i < nodes.length; i++) {
+        nodes[i].parentNode.removeChild(nodes[i]);
+    }
+    let gridSize = prompt("Please Enter a Number");
+    makeGrid(gridSize, gridSize);
+}
+
 makeGrid(16, 16)
+
+gridButton.addEventListener("click", changeGridSize);
 
 container.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "black";
-})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
